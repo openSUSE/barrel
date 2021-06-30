7cea7efaa6ca8b932a379b75d8374f0034af4c28
Arvin Schnell <aschnell@suse.de>
2021-06-30 09:16:23 +0200

    - allow trailing newlines in json parser

--- barrel/Utils/JsonFile.cc
+++ barrel/Utils/JsonFile.cc
@@ -20,12 +20,15 @@
  */
 
 
-#include <functional>
-#include <memory>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
 #include <stdexcept>
-#include <fstream>
 
 #include "JsonFile.h"
+#include "Text.h"
 
 
 namespace barrel
@@ -34,28 +37,46 @@ namespace barrel
     JsonFile::JsonFile(const string& filename)
 	: root(nullptr)
     {
-	std::unique_ptr<json_tokener, std::function<void(json_tokener*)>> tokener(
-	    json_tokener_new(), [](json_tokener* p) { json_tokener_free(p); }
-	);
-
-	ifstream file(filename);
-	if (!file.good())
-	    throw runtime_error("opening json file failed");
-
-	file.unsetf(ifstream::skipws);
-
-	string line;
-	getline(file, line);
-	while (file.good())
+	int fd = open(filename.c_str(), O_RDONLY);
+	if (fd < 0)
 	{
-	    root = json_tokener_parse_ex(tokener.get(), line.c_str(), line.size());
-
-	    json_tokener_error jerr = json_tokener_get_error(tokener.get());
-	    if (jerr != json_tokener_success && jerr != json_tokener_continue)
-		throw runtime_error("json parser failed");
-
-	    getline(file, line);
+	    throw runtime_error(sformat("opening json file '%s' failed", filename.c_str()));
 	}
+
+	struct stat st;
+	if (fstat(fd, &st) < 0)
+	{
+	    close(fd);
+	    throw runtime_error(sformat("stat for json file '%s' failed", filename.c_str()));
+	}
+
+	void* data = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (!data)
+	{
+	    close(fd);
+	    throw runtime_error(sformat("mmap for json '%s' file failed", filename.c_str()));
+	}
+
+	json_tokener* tokener = json_tokener_new();
+	root = json_tokener_parse_ex(tokener, (const char*) data, st.st_size);
+
+	munmap(data, st.st_size);
+	close(fd);
+
+	if (json_tokener_get_error(tokener) != json_tokener_success)
+	{
+	    json_tokener_free(tokener);
+	    throw runtime_error(sformat("parsing json file '%s' failed", filename.c_str()));
+	}
+
+	// excessive content
+	if (tokener->char_offset != st.st_size)
+	{
+	    json_tokener_free(tokener);
+	    throw runtime_error(sformat("excessive content in json file '%s'", filename.c_str()));
+	}
+
+	json_tokener_free(tokener);
     }
 
 
