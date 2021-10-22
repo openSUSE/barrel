fc62243735809370eb03dd1834111d4632cc8a63
Arvin Schnell <aschnell@suse.de>
2021-10-22 16:31:51 +0200

    - size option is required for create lv command

--- barrel/create-lvm-lv.cc
+++ barrel/create-lvm-lv.cc
@@ -143,11 +143,11 @@ namespace barrel
 	    if (!LvmLv::is_valid_lv_name(lv_name))
 		throw OptionsException("invalid logical volume name for command 'lv'");
 
-	    if (parsed_opts.has_option("size"))
-	    {
-		string str = parsed_opts.get("size");
-		size = SmartSize(str);
-	    }
+	    if (!parsed_opts.has_option("size"))
+		throw OptionsException("size missing for command 'lv'");
+
+	    string str = parsed_opts.get("size");
+	    size = SmartSize(str);
 
 	    if (parsed_opts.has_option("stripes"))
 	    {
