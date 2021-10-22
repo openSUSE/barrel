99d1dda661a1687746ed8ec8010e09455d581424
Arvin Schnell <aschnell@suse.de>
2021-10-22 08:54:52 +0200

    - added testcase for misuse

---
+++ testsuite/misuse1.cc
@@ -0,0 +1,108 @@
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE barrel
+
+#include <numeric>
+#include <boost/test/unit_test.hpp>
+
+#include <storage/Actiongraph.h>
+
+#include "../barrel/handle.h"
+#include "../barrel/Utils/Args.h"
+
+
+using namespace std;
+using namespace storage;
+using namespace barrel;
+
+
+namespace std
+{
+    ostream& operator<<(ostream& s, const vector<string>& lines)
+    {
+	for (const string& line : lines)
+	    s << line << '\n';
+
+	return s;
+    }
+}
+
+
+pair<string, string>
+huhu(int argc, char** argv, const Testsuite* testsuite)
+{
+    ostringstream buffer1;
+    ostringstream buffer2;
+
+    streambuf* old1 = cout.rdbuf(buffer1.rdbuf());
+    streambuf* old2 = cerr.rdbuf(buffer2.rdbuf());
+
+    handle(argc, argv, testsuite);
+
+    cout.rdbuf(old1);
+    cerr.rdbuf(old2);
+
+    return make_pair(buffer1.str(), buffer2.str());
+}
+
+
+BOOST_AUTO_TEST_CASE(test1)
+{
+    // The block devices are missing. That is detected before probing.
+
+    Args args({ "barrel", "--dry-run", "create", "raid5" });
+
+    vector<string> output1 = {
+	// No "Probing..."
+    };
+
+    vector<string> output2 = {
+	"error: block devices missing for command 'raid'"
+    };
+
+    Testsuite testsuite;
+    testsuite.devicegraph_filename = "empty1.xml";
+
+    pair<string, string> lhs = huhu(args.argc(), args.argv(), &testsuite);
+
+    string rhs1 = accumulate(output1.begin(), output1.end(), ""s,
+			     [](auto a, auto b) { return a + b + "\n"; });
+
+    BOOST_CHECK_EQUAL(lhs.first, rhs1);
+
+    string rhs2 = accumulate(output2.begin(), output2.end(), ""s,
+			     [](auto a, auto b) { return a + b + "\n"; });
+
+    BOOST_CHECK_EQUAL(lhs.second, rhs2);
+}
+
+
+BOOST_AUTO_TEST_CASE(test2)
+{
+    // The --size option is missing. That is detected before probing.
+
+    Args args({ "barrel", "--dry-run", "create", "vg", "--name", "test", "--pool", "HDDs (512 B)" });
+
+    vector<string> output1 = {
+	// No "Probing..."
+    };
+
+    vector<string> output2 = {
+	"error: size argument required for command 'vg'"
+    };
+
+    Testsuite testsuite;
+    testsuite.devicegraph_filename = "empty1.xml";
+
+    pair<string, string> lhs = huhu(args.argc(), args.argv(), &testsuite);
+
+    string rhs1 = accumulate(output1.begin(), output1.end(), ""s,
+			     [](auto a, auto b) { return a + b + "\n"; });
+
+    BOOST_CHECK_EQUAL(lhs.first, rhs1);
+
+    string rhs2 = accumulate(output2.begin(), output2.end(), ""s,
+			     [](auto a, auto b) { return a + b + "\n"; });
+
+    BOOST_CHECK_EQUAL(lhs.second, rhs2);
+}
