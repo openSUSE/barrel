63e08092c500a859cc69a005d6aa04fe4ca9369a
Arvin Schnell <aschnell@suse.de>
2024-07-11 11:06:23 +0200

    - added activate-by option for encryption

--- barrel/create-filesystem.cc
+++ barrel/create-filesystem.cc
@@ -189,7 +189,7 @@ namespace barrel
 
 		map<string, MountByType>::const_iterator it = str_to_mount_by_type.find(str);
 		if (it == str_to_mount_by_type.end())
-		    throw runtime_error(sformat(_("unknown mount-by type type '%s'"), str.c_str()));
+		    throw runtime_error(sformat(_("unknown mount-by type '%s'"), str.c_str()));
 
 		mount_by = it->second;
 	    }
